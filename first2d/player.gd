dae2252e13925126a43ac8f9e9568e59ca222d4d
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-01-05 13:41:50 +0100

    Add collision to the player
    Add collision to the player with the use of a signal.

--- first2d/player.gd
+++ first2d/player.gd
@@ -1,10 +1,12 @@
 extends Area2D
+signal hit
 @export var speed = 400 # How fast the player will move (pixels/sec)
 var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,6 +26,26 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y >	0
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body):
+	hide() #Player disappears after being hit
+	hit.emit()
+	#Must be deferred as we cannot change physics properties on a physics call back
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
