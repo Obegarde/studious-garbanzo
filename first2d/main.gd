332019d9e773723cb6db3d2cc6c002361a32a22e
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-01-12 11:31:27 +0100

    Add hud elements and Fix mob spawn position
    Add score, titlecard and start button. As well as game over information.
    Fix the bug of mobs only spawning at the same location in the top left.
    Now they spawn along the 2DPath that goes all round the play area.

--- first2d/main.gd
+++ first2d/main.gd
@@ -1,12 +1,12 @@
 extends Node
 
+
 @export var mob_scene: PackedScene
 var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,12 +16,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene
@@ -34,12 +37,16 @@ func _on_mob_timer_timeout():
 	#Set the mobs direction perpendicular to the path direction.
 	var direction = mob_spawn_location.rotation + PI / 2
 	
+	#Set the mobs position to a random location
+	mob.position = mob_spawn_location.position
+	
 	#Add some randomness to the direction
 	direction += randf_range(-PI / 4 , PI / 4)
 	mob.rotation = direction
 	
 	#Choose the velocity of the mob
 	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
 	
 	#Spawn the mob by adding it as a child to the Main scene.
 	add_child(mob)
@@ -47,8 +54,10 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
